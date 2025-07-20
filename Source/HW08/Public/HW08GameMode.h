1513b274285691dadb0356c7b2d39b4eb2385ef9
chaenning <codmsrjf@naver.com>
2025-07-20 23:29:08 +0900

    update
    GameMode 추가
    캐릭터 에셋 추가

---
+++ Source/HW08/Public/HW08GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "HW08GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HW08_API AHW08GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
